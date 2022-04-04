73e3f7ecea46eac8d6a986613e92a110fbdb492f
mjamry <michal.jamry@gmail.com>
2022-04-04 16:20:27 +0200

    Added improvements in the page UI and styles

--- ClientApp/src/QuotePage.tsx
+++ ClientApp/src/QuotePage.tsx
@@ -1,7 +1,25 @@
-/* eslint-disable react/no-this-in-sfc */
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useEffect, useState } from 'react';
+import michael from './assets/5e93b4a43af44260882e33b0.png';
+import andy from './assets/5e93b5453af44260882e33b9.png';
+import jim from './assets/5e93b4f03af44260882e33b1.png';
+import angela from './assets/5e93b52b3af44260882e33b6.png';
+import dwight from './assets/5e93b4fa3af44260882e33b2.png';
+import pam from './assets/5e93b50a3af44260882e33b3.png';
+import ryan from './assets/5e93b5183af44260882e33b4.png';
+import erin from './assets/5e93b5653af44260882e33bd.png';
+import kevin from './assets/5e93b5323af44260882e33b7.png';
+
+const characters = {
+  '5e93b4a43af44260882e33b0': michael,
+  '5e93b5453af44260882e33b9': andy,
+  '5e93b4f03af44260882e33b1': jim,
+  '5e93b52b3af44260882e33b6': angela,
+  '5e93b4fa3af44260882e33b2': dwight,
+  '5e93b50a3af44260882e33b3': pam,
+  '5e93b5183af44260882e33b4': ryan,
+  '5e93b5653af44260882e33bd': erin,
+  '5e93b5323af44260882e33b7': kevin,
+};
 
 type ResponseData = {
   quote: string;
@@ -33,21 +51,25 @@ function QuotePage() {
   return (
     <div className="container">
       <div className="card">
-        <div className="characterHeaderContainer">
-          <div className="characterHeader">
-            {state.characterId}
-          </div>
-        </div>
         <div className="quote">
           {state.quote}
         </div>
-        <div className="characterName">
-          {state.character}
+        <div className="characterHeaderContainer">
+          <div className="characterHeader">
+            <img src={characters[state.characterId]} alt="" />
+          </div>
+        </div>
+        <div className="characterNameContainer">
+          <div className="characterName">
+            {state.character}
+          </div>
         </div>
       </div>
       <div
         className="button"
         onClick={() => getData()}
+        onKeyDown={() => getData()}
+        role="none"
       >
         Shuffle
       </div>
